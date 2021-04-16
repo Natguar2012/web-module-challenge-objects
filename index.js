d946552468d0b23ed9a6723f463f85ea64cdd7e4
Natguar2012 <78192708+Natguar2012@users.noreply.github.com>
2021-04-16 01:40:30 -0500

    finished task 3

--- index.js
+++ index.js
@@ -50,18 +50,18 @@ Using the burger object below do the following:
 
   For example: burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2
 */
-// export const burger = {
-//   name: "Burger", 
-//   price: 18, 
-//   category: "Lunch", 
-//   discount: function(customer){
-//     if(customer === 'teacher' || customer === 'student'){
-//       return this.price * (0.75)
-//     }
-//     else this.price * (0.90)
-// }
-// }
-// console.log(burger.discount('teacher'));
+export const burger = {
+  name: "Burger", 
+  price: 18, 
+  category: "Lunch", 
+  discount: function(customer){
+    if(customer === 'teacher' || customer === 'student'){
+      return this.price * (0.75)
+    }
+    else this.price * (0.90)
+}
+}
+console.log(burger.discount('teacher'));
 
 
 ///////////////Reviews (MVP)///////////////////
@@ -81,6 +81,7 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
+console.log(reviews[5].feedback);
 
 
 
@@ -89,6 +90,13 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+
+const newObject = { 
+  name: "Wesley",
+  rating: 5,
+  feedback: "I love this place! They have the best drinks."
+}
+
 
 
 
@@ -112,26 +120,15 @@ Use the getReviewByIndex function below to do the following:
   For example: getReviewByIndex(reviews,0) would return: "Daniela gave the restaurant a 5 star review, and their feedback was: Beautiful atmosphere and wonderful vegan options!"
 */
 
-// const reviews = [
-//   {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
-//   {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
-//   {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-//   {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
-//   {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
-//   {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
-//   {name: "Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
-//   {name: "Reyna", rating: 3.5, feedback: ""},
-// ]
-
-// function getReviewByIndex(array, index){
-//     let theObject = {
-//       name: "Daniela",
-//       rating: "5",
-//       feedback: "Beautiful"
-//     }
-//   return `${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was ${array[index].feedback}`
-//     }
-// console.log(getReviewByIndex(reviews,3));
+function getReviewByIndex(array, index){
+    let theObject = {
+      name: "Daniela",
+      rating: "5",
+      feedback: "Beautiful"
+    }
+  return `${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was ${array[index].feedback}`
+    }
+console.log(getReviewByIndex(reviews,3));
 
 
   
@@ -224,9 +221,9 @@ function foo(){
   return 'bar';
 }
 
-// export default{
-//   foo,
-//   createMenuItem,
-//   getReviewByIndex,
-//   getLastReview,
-// }
+export default{
+  foo,
+  createMenuItem,
+  getReviewByIndex,
+  getLastReview,
+}
